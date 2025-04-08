4615e5aff828e74dcd330ec8203507d2cd5ed390
amina-taleb <amina.taleb@laplateforme.io>
2025-04-08 12:09:00 +0200

    add the function (rapide)

--- sorting.py
+++ sorting.py
@@ -23,9 +23,9 @@ def insertion(liste) :
 def selection(liste) :
     n = len(liste)
     for i in range(n) :
-        min_index = i    # on suppose que i est l'indice du plus petit nombre (on le fixe)
-        for j in range(i + 1, n) :  # on le pointe sur le reste de la liste et on cherche le min
-            if liste[j] < liste[min_index] :
+        min_index = i    # on suppose que i est l'indice du plus petit nombre (on le fixe) = selectionner un element et le stocker dans une variable temporaire
+        for j in range(i + 1, n) :  # on parcourt la sous-liste qui reste (i+1 a n)
+            if liste[j] < liste[min_index] : #on chercher le min 
                 min_index = j # maintenant j devient l'indice du plus petit element
         liste[i], liste[min_index] = liste[min_index], liste[i]   #on échange les positions de l'élement d'indice i (qui se trouve au début de la liste) avec le plus petit élement de la liste non triées
     return liste
@@ -40,9 +40,22 @@ def fusion(liste) :
 
 #####################################################################################################################################################################
 def rapide(liste) :
-    print(f"")
+    n = len(liste)
+    if n <= 1 :   # il y a un seul element dans la liste ou une liste vide
+        return liste 
+    pivot = liste[-1]   #par convention on choisit le dernier element
+    # ou bien : pivot = liste.pop() une fonction qui renvoit le dernier element
+    liste_inf = []
+    liste_sup = []   #créer deux listes
+    for i in range (0, n-1) :
+        if liste[i] <= pivot :
+            liste_inf.append(liste[i])
+        else :
+            liste_sup.append(liste[i])
+
+    return rapide(liste_inf) + [pivot] + rapide(liste_sup)
 
 #####################################################################################################################################################################
 #exemple : 
 liste = [8, 2, 6, 9, 5, 6, 0, 4, 7, 1]
-print(insertion(liste))
+print(rapide(liste))
